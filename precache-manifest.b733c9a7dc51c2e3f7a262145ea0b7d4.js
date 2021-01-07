fe9de52f38fa31907d3c887dd8e799ac8ceeec8c
Erdoo <erdoo.kuhwa@gmail.com>
2021-01-07 07:50:49 -0600

    Updates

---
+++ precache-manifest.b733c9a7dc51c2e3f7a262145ea0b7d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee828bfca26e476c86cee06b55652167",
+    "url": "/kweez/index.html"
+  },
+  {
+    "revision": "93e34f954cce451da83c",
+    "url": "/kweez/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "cba4e48e0f2069ba97c3",
+    "url": "/kweez/static/css/main.0a013637.chunk.css"
+  },
+  {
+    "revision": "93e34f954cce451da83c",
+    "url": "/kweez/static/js/2.4b1f8be8.chunk.js"
+  },
+  {
+    "revision": "4cc72640cc952888d87a66a4266d3ebc",
+    "url": "/kweez/static/js/2.4b1f8be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba4e48e0f2069ba97c3",
+    "url": "/kweez/static/js/main.ccefc00d.chunk.js"
+  },
+  {
+    "revision": "ddc7c5b3ff718bdbf722",
+    "url": "/kweez/static/js/runtime-main.05c60285.js"
+  }
+]);
