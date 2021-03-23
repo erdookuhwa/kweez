d69733cbc56c7d47f8965dd9ce31c17688ee016e
Erdoo <erdoo.kuhwa@gmail.com>
2021-03-23 13:23:07 -0500

    Updates

---
+++ precache-manifest.5b25f328f91df344bca62250f62740ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcdafacc60c1079f72054d10e46945f",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/index.html"
+  },
+  {
+    "revision": "d43fa1caa10615a63043",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "e02593e28e973a973663",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/css/main.0a013637.chunk.css"
+  },
+  {
+    "revision": "d43fa1caa10615a63043",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/js/2.84a1adaf.chunk.js"
+  },
+  {
+    "revision": "4cc72640cc952888d87a66a4266d3ebc",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/js/2.84a1adaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02593e28e973a973663",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/js/main.5ee01484.chunk.js"
+  },
+  {
+    "revision": "535cea362662fc274620",
+    "url": "/erdookuhwa/react-quiz-gh-pages.git/static/js/runtime-main.976ac1a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/erdookuhwa/my-quiz.git/precache-manifest.9e2d02e1060714dba067d5db22e2e567.js"
+  "/erdookuhwa/react-quiz-gh-pages.git/precache-manifest.5b25f328f91df344bca62250f62740ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/erdookuhwa/my-quiz.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/erdookuhwa/react-quiz-gh-pages.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
