d801a11b9c66737044aa95962d86fe87ac157eab
Erdoo <erdoo.kuhwa@gmail.com>
2021-03-23 13:26:34 -0500

    Updates

---
+++ precache-manifest.1e0d94fc99759b2701ac6f66b0f7a2a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542efd571a14a48e13d19c10e400b03c",
+    "url": "/react-quiz-gh-pages/index.html"
+  },
+  {
+    "revision": "c1f2ecba9f335fe8a471",
+    "url": "/react-quiz-gh-pages/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "e02593e28e973a973663",
+    "url": "/react-quiz-gh-pages/static/css/main.0a013637.chunk.css"
+  },
+  {
+    "revision": "c1f2ecba9f335fe8a471",
+    "url": "/react-quiz-gh-pages/static/js/2.67b831a5.chunk.js"
+  },
+  {
+    "revision": "4cc72640cc952888d87a66a4266d3ebc",
+    "url": "/react-quiz-gh-pages/static/js/2.67b831a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02593e28e973a973663",
+    "url": "/react-quiz-gh-pages/static/js/main.5ee01484.chunk.js"
+  },
+  {
+    "revision": "3af86b0bfc87acb49aef",
+    "url": "/react-quiz-gh-pages/static/js/runtime-main.24c3c2ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/erdookuhwa/react-quiz-gh-pages.git/precache-manifest.5b25f328f91df344bca62250f62740ad.js"
+  "/react-quiz-gh-pages/precache-manifest.1e0d94fc99759b2701ac6f66b0f7a2a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/erdookuhwa/react-quiz-gh-pages.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-quiz-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
